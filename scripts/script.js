d85f8b72742092bd9b1004adfdab5d9679e72ecb
eqz.nava@gmail.com <eqz.nava@gmail.com>
2021-11-22 17:04:33 -0600

    Making the responsive design

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let randomNumber = Math.floor(Math.random()*100)+1;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
@@ -7,67 +7,67 @@ const lowOrHi = document.querySelector('.lowOrHi');
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 
-let guessCount =1;
+let guessCount = 1;
 let resetButton;
 guessField.focus();
 
-function checkGuess(){
-  let userGuess = Number(guessField.value);
-  if (guessCount === 1) {
-    guesses.textContent = 'Intentos anteriores: ';
-  }
-  guesses.textContent += userGuess + ' ';
-
-  if (userGuess === randomNumber) {
-    lastResult.textContent = '¡Felicidades! ¡Lo adivinaste!';
-    lastResult.style.backgroundColor = 'green';
-    lowOrHi.textContent = '';
-    setGameOver();
-  } else if(guessCount === 10){
-    lastResult.textContent = '!!!Fin del Juego¡¡¡'
-    setGameOver();
-  }else {
-    lastResult.textContent = '¡Incorrecto!';
-    lastResult.style.backgroundColor = 'red';
-    if (userGuess < randomNumber) {
-      lowOrHi.textContent = '¡El numero esta muy bajo!';
-    }else if (userGuess > randomNumber) {
-      lowOrHi.textContent = '¡El numero esta muy arriba!';
+function checkGuess() {
+    let userGuess = Number(guessField.value);
+    if (guessCount === 1) {
+        guesses.textContent = 'Intentos anteriores: ';
     }
-  }
+    guesses.textContent += userGuess + ' ';
 
-  guessCount++;
-  guessField.value ='';
-  guessField.focus();
+    if (userGuess === randomNumber) {
+        lastResult.textContent = '¡Felicidades! ¡Lo adivinaste!';
+        lastResult.style.backgroundColor = 'green';
+        lowOrHi.textContent = '';
+        setGameOver();
+    } else if (guessCount === 10) {
+        lastResult.textContent = '!!!Fin del Juego¡¡¡'
+        setGameOver();
+    } else {
+        lastResult.textContent = '¡Incorrecto!';
+        lastResult.style.backgroundColor = 'red';
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = '¡El numero esta muy bajo!';
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = '¡El numero esta muy arriba!';
+        }
+    }
+
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
 }
 
 guessSubmit.addEventListener('click', checkGuess);
 
-function setGameOver(){
-  guessField.disabled = true;
-  guessSubmit.disabled = true;
-  resetButton = document.createElement('button');
-  resetButton.textContent = 'Jugar otra vez';
-  document.body.append(resetButton);
-  resetButton.addEventListener('click', resetGame);
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Jugar otra vez';
+    document.body.append(resetButton);
+    resetButton.addEventListener('click', resetGame);
 }
 
-function resetGame(){
-  guessCount = 1;
+function resetGame() {
+    guessCount = 1;
 
-  const resetParas = document.querySelectorAll('.resultParas p');
-  for (let i = 0; i < resetParas.length; i++) {
-    resetParas[i].textContent = '';
-  }
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for (let i = 0; i < resetParas.length; i++) {
+        resetParas[i].textContent = '';
+    }
 
-  resetButton.parentNode.removeChild(resetButton);
+    resetButton.parentNode.removeChild(resetButton);
 
-  guessField.disabled = false;
-  guessSubmit.disabled = false;
-  guessField.value = '';
-  guessField.focus();
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
 
-  lastResult.style.backgroundColor = 'white';
+    lastResult.style.backgroundColor = 'white';
 
-  randomNumber = Math.floor(Math.random() * 100) + 1;
+    randomNumber = Math.floor(Math.random() * 100) + 1;
 }
